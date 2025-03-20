e03531758efb351327fe18e9eb349ad93ec32bd6
Your Name <you@example.com>
2025-03-20 19:13:09 +0700

    getstorage

--- lib/app3/controllers/translate_controller.dart
+++ lib/app3/controllers/translate_controller.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class TranslateController extends GetxController {
-  var currentLocale = const Locale('en').obs;
+  final storage = GetStorage();
+  Rx<Locale> currentLocale = const Locale('en').obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    String? change = storage.read('language');
+    if (change != null) {
+      currentLocale.value = Locale(change);
+    }
+  }
 
   void changeLanguage() {
-    if (currentLocale.value.languageCode == 'kh') {
-      currentLocale.value = const Locale('en');
-    } else {
-      currentLocale.value = const Locale('kh');
-    }
+    String newLang = currentLocale.value.languageCode == "en" ? "kh" : "en";
+
+    currentLocale.value = Locale(newLang);
+    storage.write('language', newLang);
     Get.updateLocale(currentLocale.value);
+    update();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,13 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:get_storage/get_storage.dart';
-// import 'package:getx_crud/app1/controllers/language_controller.dart';
-// import 'package:getx_crud/app1/controllers/theme_controller.dart';
-// import 'package:getx_crud/app1/translate/my_translate.dart';
-// import 'package:getx_crud/app1/views/home_page.dart';
-
-// void main() async {
-//   await GetStorage.init();
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   final LanguageController langController = Get.put(LanguageController());
-//   final ThemeController themeController = Get.put(ThemeController());
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Obx(() => GetMaterialApp(
-//           debugShowCheckedModeBanner: false,
-//           translations: MyTranslations(),
-//           locale: langController.currentLocale.value,
-//           fallbackLocale: Locale('en'),
-//           theme: ThemeData.light(),
-//           darkTheme: ThemeData.dark(),
-//           themeMode: themeController.isDarkMode.value
-//               ? ThemeMode.dark
-//               : ThemeMode.light,
-//           home: HomePage(),
-//         ));
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:getx_crud/app1/controllers/theme_controller.dart';
 import 'package:getx_crud/app3/controllers/translate_controller.dart';
 import 'package:getx_crud/app3/pages/home_page.dart';
 import 'package:getx_crud/app3/translate/translation.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(MyApp());
 }
 
